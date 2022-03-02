a8bd4c42cc7ae95ff45486e9777ce5d249b77aa9
Mohd ShahMukit Alam <85111454+msalam156@users.noreply.github.com>
2022-03-03 04:15:39 +0530

    Update index.js

--- Tasks/March/Week1/Assignment-2/SumOfNumber/Code/index.js
+++ Tasks/March/Week1/Assignment-2/SumOfNumber/Code/index.js
@@ -2,13 +2,13 @@ function Add() {
     var input1 = document.getElementById("take1").value;
     var input2 = document.getElementById("take2").value;
     if(isNaN(input1,input2)){
-        document.getElementById('h1').innerText=(`soryy I can't perform '${input1}' and '${input2}'`);
+        document.getElementById('h1').innerHTML=(`soryy I can't perform '${input1}' and '${input2}'`);
     }
     else {
         let data1 = parseInt(input1)
         let data2 = parseInt(input2)
         let sum = data1 + data2
-        document.getElementById('h1').innerText=sum;
+        document.getElementById('h1').innerHTML=sum;
     }
     
 }
